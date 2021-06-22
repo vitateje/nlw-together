788ac40abc690e5602bf3649a9a14c5fdfa63009
vitateje <vitateje@gmail.com>
2021-06-21 23:03:25 -0300

    aula 02 - maximum speed

--- src/server.ts
+++ src/server.ts
@@ -1,42 +1,13 @@
+import "reflect-metadata";
 import express from "express";
+import { router } from "./routes"
 
-// @types/express
+import "./database";
+
 const app = express();
 
-/**
- * GET    => Buscar uma informação
- * POST   => Inserir (Criar) uma informação
- * PUT    => Alterar uma informação
- * DELETE => Remover um dado
- * PATCH  => Alterar uma informação específica
- */
+app.use(express.json());
 
-/**
- * Tipos de parâmetros
- * Routes Params => http://localhost:3000/produtos/8947589789
- * QUery Params => http://localhost:3000/produtos?name=teclado&description=tecladobom
- * 
- * Body Params => {
- * "name": "teclado",
- * "description": "teclado bom"
- * }
- */
+app.use(router);
 
-app.get("/test", (request, response) => {
-  // Request => Entrando
-  // Response => Saindo
-  return response.send("Olá NLW");
-});
-
-app.get("/test-hack", (request, response) => {
-  // Request => Entrando
-  // Response => Saindo
-  return response.send("Hacking NASA");
-});
-
-app.post("/test-post", (request, response) => {
-  return response.send("Olá NLW método POST");
-});
-
-// http://localhost:3000
 app.listen(3000, () => console.log("Server is running"));
