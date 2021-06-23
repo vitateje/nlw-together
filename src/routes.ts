0283e06f139a86ac4949d2630c0e25b6ddf3de5a
vitateje <vitateje@gmail.com>
2021-06-22 21:38:21 -0300

    tags - exception - middlewares

--- src/routes.ts
+++ src/routes.ts
@@ -1,11 +1,16 @@
 import { Router } from "express";
 import { CreateUserController } from "./controllers/CreateUserController";
-
+import { CreateTagController } from "./controllers/CreateTagController";
+import { ensureAdmin } from "./middlewares/ensureAdmin";
 
 const router = Router();
 
 const createUserController = new CreateUserController();
+const createTagController = new CreateTagController();
 
 router.post("/users", createUserController.handle);
+
+// middlewares: fica entre a requisição e a resposta - ensureAdmin ( pode interceptar ou seguir a frente )
+router.post("/tags", ensureAdmin, createTagController.handle);
 
 export { router };
