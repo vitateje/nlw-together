7ce28707a189b1abc6134669fd05abd9bca6ad2f
vitateje <vitateje@gmail.com>
2024-03-13 21:20:41 -0300

    update

--- src/controllers/ListUsersController.ts
+++ src/controllers/ListUsersController.ts
@@ -4,9 +4,17 @@ import { ListUserService } from "../services/ListUsersService";
 
 class ListUsersController {
     async handle( request: Request, response: Response) {
+
+
         const listUserService = new ListUserService();
 
         const users = await listUserService.execute();
+
+        // console.log(users)
+
+        // const userItems = users.filter( User => User.id === "f8f8c2a3-14b1-4510-884a-7288de871866");
+
+        // console.log(userItems)
 
         return response.json(users);
     }
